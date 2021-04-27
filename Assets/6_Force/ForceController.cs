5b984e2f17f172dca92957addcc942a875d7a628
Choi-UngGyu <cdr982116@gmail.com>
2021-04-28 00:28:27 +0900

    AddForce를 활용한 게임 이해

---
+++ Assets/6_Force/ForceController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceController : MonoBehaviour
+{
+    private Rigidbody boxRigidbody;
+    private float movePower = 5f;
+    void Start()
+    {
+        boxRigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(boxRigidbody.velocity);
+        if (Input.GetKeyDown(KeyCode.A))
+            boxRigidbody.AddForce(transform.right * movePower, ForceMode.Impulse);
+        // boxRigidbody.AddForce(transform.right * movePower / Time.fixedDeltaTime, ForceMode.Force);
+        // 둘이 똑같음
+        else if (Input.GetKey(KeyCode.S))
+            boxRigidbody.AddForce(transform.right * movePower, ForceMode.Force);
+        else if (Input.GetKeyDown(KeyCode.D))
+            boxRigidbody.AddForce(transform.right * movePower, ForceMode.VelocityChange);
+        // boxRigidbody.AddForce(transform.right * movePower / Time.fixedDeltaTime, ForceMode.Acceleration);
+        // 둘이 똑같음
+        else if (Input.GetKey(KeyCode.F))
+            boxRigidbody.AddForce(transform.right * movePower, ForceMode.Acceleration);
+    }
+}
