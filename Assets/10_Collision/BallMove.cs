06086afefa027fa2a2ac5ca21afc0f1c47c9777d
Choi-UngGyu <cdr982116@gmail.com>
2021-04-28 22:47:46 +0900

    탄성 충돌에 관한 게임 요소 구현

---
+++ Assets/10_Collision/BallMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    private Rigidbody2D ballRB2D;
+    private float moveVelocity = 5f;
+    void Start()
+    {
+        ballRB2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            ballRB2D.velocity = transform.right * moveVelocity;
+    }
+}
