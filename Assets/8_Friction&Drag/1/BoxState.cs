e24def10884ed2c100e068aa9fe125453415504f
Choi-UngGyu <cdr982116@gmail.com>
2021-04-28 22:25:18 +0900

    마찰력&저항력을 활용한 게임 요소 구현

---
+++ Assets/8_Friction&Drag/1/BoxState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxState : MonoBehaviour
+{
+    public PhysicsMaterial2D slopeMaterial;
+    private Rigidbody2D boxRigidbody2D;
+
+    private float boxMass = 0f;
+    private float gravity = 0f;
+    private float friction = 0f;
+    private float angle = 0f;
+
+    void Start()
+    {
+        boxRigidbody2D = GetComponent<Rigidbody2D>();
+        boxMass = boxRigidbody2D.mass; // 질량값
+        gravity = 9.8f * boxRigidbody2D.gravityScale; // 중력값
+        friction = slopeMaterial.friction; // 마찰계수
+        angle = transform.rotation.eulerAngles.z;
+
+        float pushForce = boxMass * gravity * Mathf.Sin(angle * Mathf.Deg2Rad);
+        float frictionForce = friction * boxMass * gravity * Mathf.Cos(angle * Mathf.Deg2Rad);
+
+        Debug.Log("Push: " + pushForce + " , Friction: " + frictionForce);
+
+        if (pushForce > frictionForce)
+            Debug.Log("움직임");
+        else
+            Debug.Log("정지");
+    }
+}
---
+++ Assets/8_Friction&Drag/2/DragController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Drag { Drag, Simulation };
+public class DragController : MonoBehaviour
+{
+    public Drag ballMode;
+    public float dragValue;
+    private Rigidbody2D ballRigidbody2D;
+    void Start()
+    {
+        ballRigidbody2D = GetComponent<Rigidbody2D>();
+        ballRigidbody2D.velocity = transform.right;
+    }
+
+    void Update()
+    {
+        if (ballMode == Drag.Simulation)
+            ballRigidbody2D.velocity = ballRigidbody2D.velocity * (1 - dragValue * Time.deltaTime);
+
+    }
+}
