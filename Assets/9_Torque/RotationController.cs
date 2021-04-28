96b2f6538553fef77ef55acef7e21da4a5075757
Choi-UngGyu <cdr982116@gmail.com>
2021-04-28 22:35:16 +0900

    회전 운동을 활용한 게임 구현

---
+++ Assets/9_Torque/RotationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    private Rigidbody2D rotationRB2D;
+    private float movePower = 5f;
+    void Start()
+    {
+        rotationRB2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rotationRB2D.AddForce(transform.right * movePower, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rotationRB2D.AddTorque(movePower, ForceMode2D.Impulse);
+        }
+    }
+}
